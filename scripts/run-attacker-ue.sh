40b92df60975865f8b04cf12cda659de7fce9c05
Abdallah Abouhasna <abouhasna8@gmail.com>
2026-05-14 20:19:07 +0300

    Add run-attacker-ue.sh script for attacker UE with SDR

---
+++ scripts/run-attacker-ue.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+cd ~/rrc-storm-attack/cmake_targets/ran_build/build
+
+sudo ./nr-uesoftmodem \
+  --C 3619200000 \
+  -r 106 \
+  --numerology 1 \
+  --ssb 516 \
+  -E \
+  --band 78 \
+  --ue-fo-compensation \
